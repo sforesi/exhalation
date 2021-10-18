1386c01d130fd9eed6ec0ae4e26faba8126bf14d
sforesi <sebastiano.foresi@gmail.com>
2021-10-18 14:32:05 -0400

    redefine submit/starButtons

--- main.js
+++ main.js
@@ -10,53 +10,52 @@ let story = {
     text: "Back again? You move through a packed marketplace, navigating your way through the busy traffic. You briefly stop to talk with a fruit vendor, who greets you kindly and hands you a date. You accept graciously and continue through the marketplace. You cut through the crowd and come to stop at a curious shop fronted by massive bronze and silver metalworks. Do you enter?",
     choices: [
       {
-        choice: "You window-shop for a while, but ultimately decide it’s all a little too gaudy for your taste.",
-        destination: 'gaudy'
+        choice: "Your interest is peaked, you walk in.",
+        destination: 'enter',
       },
       {
-        choice: "Your interest is peaked, you walk in.",
-        destination: 'enter'
+        choice: "You window-shop for a while, but ultimately decide it’s all a little too gaudy for your taste.",
+        destination: 'gaudy',
       },
       {
         choice: "No, you walk away. Chooising to save adventure for another day.",
-        destination: 'disinterested' 
-      },
+        destination: 'disinterested', 
+      }
     ]
-},
-gaudy: {
-  title: "Not your taste...",
-  text: "Your snobbery leads you astray once again, you lose. And perhaps more tragically, you've missed out on adventure.",
-  }
-},
-enter: {
-  title: "A curious mind!",
-  text: "You enter the shop, which HUMS with the whirs and clanks of gears and mechanisms. At the entrance sits an astrolabe inlaid in silver, so large it could be the face of a clock tower. A water-clock sounds, its chime WHISTLED by a swallow that seems to raise itself out the water. A figure steps out from what seems to be an infinite doorway. Their eyes are kind and familiar, their voice warm. They greet you like an old friend.",
-  choices: [
-    {
-      choice:'',
-    },
-    {
-      choice:'',
-    },
-    {
-      choice:'',
-    },
-  ]
   }
 }
 
+let enter= {
+  title: "A curious mind!",
+  text: "You enter the shop, which HUMS with the whirs and clanks of gears and mechanisms. At the entrance sits an astrolabe inlaid in silver, so large it could be the face of a clock tower. A water-clock sounds, its chime WHISTLED by a swallow that seems to raise itself out the water. A figure steps out from what seems to be an infinite doorway. Their eyes are kind and familiar, their voice warm. They greet you like an old friend. How do you respond?",
+  choices: [
+    {
+      choice:'Tell them you want to shop on your own.',
+    },
+    {
+      choice:'Hand them your resume.',
+    },
+    {
+      choice:'Ask them where they acquired their merchandise.',
+    },
+  ]
+}
+let gaudy = {
+  title: "Not your taste...",
+  text: "Your snobbery leads you astray once again, you lose. And perhaps more tragically, you've missed out on adventure.",
+}
 
 /*--------- Cached Element References ---------*/
-
+  const startButton = document.querySelector('#start-button')
+  const input = document.querySelector('#name-input')
+  const content = document.querySelector('#content')
 
 /*-------------- Event Listeners --------------*/
-document.addEventListener('DOMContentLoaded', function() {
-  let button = document.querySelector('#start-button')
-  let input = document.querySelector('#name-input')
-  let content = document.querySelector('#content')
-  button.addEventListener('click', render)
+  
+  startButton.addEventListener('click', render)
+  submitButton.addEventListener('click', function(){
+  getInputValue()
 })
-
 /*----------------- Functions -----------------*/
 
 function render() {
@@ -66,10 +65,7 @@ function render() {
   ${getInputs()}  
   <button id = "submit-button">Proceed</button>
   `
-  let button = document.querySelector("#submit-button");
-  button.addEventListener('click', function(){
-    getInputValue()
-  })
+  let submitButton = document.querySelector("#submit-button");
 }
 
 function getInputValue(){
