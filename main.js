a7dc62f788000db26fa2baad2e2569d92d75c5e7
sforesi <sebastiano.foresi@gmail.com>
2021-10-18 13:21:37 -0400

    enter destination partially complete

--- main.js
+++ main.js
@@ -22,8 +22,29 @@ let story = {
         destination: 'disinterested' 
       },
     ]
+},
+gaudy: {
+  title: "Not your taste...",
+  text: "Your snobbery leads you astray once again, you lose. And perhaps more tragically, you've missed out on adventure.",
+  }
+},
+enter: {
+  title: "A curious mind!",
+  text: "You enter the shop, which HUMS with the whirs and clanks of gears and mechanisms. At the entrance sits an astrolabe inlaid in silver, so large it could be the face of a clock tower. A water-clock sounds, its chime WHISTLED by a swallow that seems to raise itself out the water. A figure steps out from what seems to be an infinite doorway. Their eyes are kind and familiar, their voice warm. They greet you like an old friend.",
+  choices: [
+    {
+      choice:'',
+    },
+    {
+      choice:'',
+    },
+    {
+      choice:'',
+    },
+  ]
   }
 }
+
 
 /*--------- Cached Element References ---------*/
 
@@ -55,7 +76,7 @@ function getInputValue(){
   let inputs = document.querySelectorAll('input[type="radio"]');
   for (let i = 0; i < inputs.length; i++) {
     if (inputs[i].checked) {
-      console.log(inputs[i])
+      console.log(inputs[i].getAttribute('data-destination'))
     }
   }
 }
