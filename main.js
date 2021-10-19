39d302cfe9c9d95ac2eca32ff8b2c4afbbd74b80
sforesi <sebastiano.foresi@gmail.com>
2021-10-18 22:07:30 -0400

    updated getUserInput function

--- main.js
+++ main.js
@@ -2,13 +2,11 @@
 /*----------------- Constants -----------------*/
 
 /*------------- Variables (state) -------------*/
-
+//object to establish the original state of the game
 let story = {
-  // chapter: "currentScene",
-  // currentScene: {
     title: "Chapter 1",
     text: "Back again? You move through a packed marketplace, navigating your way through the busy traffic. You briefly stop to talk with a fruit vendor, who greets you kindly and hands you a date. You accept graciously and continue through the marketplace. You cut through the crowd and come to stop at a curious shop fronted by massive bronze and silver metalworks. Do you enter?",
-    choices: [
+    choices: [ // array of choices and destinations 
       {
         choice: "Your interest is peaked, you walk in.",
         destination: 'enter',
@@ -25,9 +23,9 @@ let story = {
   }
 
 
-let enter= {
+let enter= { 
   title: "A curious mind!",
-  text: "You enter the shop, which HUMS with the whirs and clanks of gears and mechanisms. At the entrance sits an astrolabe inlaid in silver, so large it could be the face of a clock tower. A water-clock sounds, its chime WHISTLED by a swallow that seems to raise itself out the water. A figure steps out from what seems to be an infinite doorway. Their eyes are kind and familiar, their voice warm. They greet you like an old friend. How do you respond?",
+  text: "You enter the shop, which hums with the whirs and clanks of gears and mechanisms. At the entrance sits an astrolabe inlaid in silver, so large it could be the face of a clock tower. A water-clock sounds, its chime whistled by a swallow that seems to raise itself out the water. A figure steps out from what seems to be an infinite doorway. Their eyes are kind and familiar, their voice warm. They greet you like an old friend. How do you respond?",
   choices: [
     {
       choice:'Tell them you want to shop on your own.',
@@ -38,11 +36,11 @@ let enter= {
     {
       choice:'Ask them where they acquired their merchandise.',
     },
-  ]
+  ]// correct or "winning" choice continues the story with more choices
 }
 let gaudy = {
   title: "Not your taste...",
-  text: "Your snobbery leads you astray once again, you lose. And perhaps more tragically, you've missed out on adventure.",
+  text: "Your snobbery leads you astray once again, you lose. And perhaps more tragically, you've missed out on adventure.", // incorrect or losing destination
 }
 
 /*--------- Cached Element References ---------*/
@@ -54,15 +52,16 @@ let gaudy = {
 /*-------------- Event Listeners --------------*/
   
   startButton.addEventListener('click', render)
-  submitButton.addEventListener('click', function(){
-  getInputValue()
+  submitButton.addEventListener('click', function() {
+  getUserInput()
 })
 
 /*----------------- Functions -----------------*/
 
+// function to render the current scene 
 function render() {
   content.innerHTML = ` 
-  <h1>${story.title}<h1>
+  <h1>${story.title}<h1> 
   <p>${story.text}</p> 
   ${getInputs()}  
   <button id = "submit-button">Proceed</button>
@@ -70,13 +69,13 @@ function render() {
   let submitButton = document.querySelector("#submit-button");
 }
 
-function getInputValue(){
+function getUserInput(){
   let inputs = document.querySelectorAll('input[type="radio"]');
   for (let i = 0; i < inputs.length; i++) {
     if (inputs[i].checked) {
       console.log(inputs[i].getAttribute('data-destination'))
-    }
-  }
+    } 
+  } // function to check user input/
 }
 
 function getInputs() {
@@ -90,5 +89,5 @@ function getInputs() {
     `    
     console.log(story.choices[i].choice)
   }
-  return input;
+  return input; // function to retrieve choices from story object
 }
